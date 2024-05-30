038af432bb849e79489e093d2bb81b2ce1c31adc
abbkane97 <kanehaby20@gmail.com>
2024-05-30 11:12:43 -0400

    Completed Sandwich drink and chips class/ have to go back and do overrides

--- src/main/java/com/pluralsight/orderitems/Chips.java
+++ src/main/java/com/pluralsight/orderitems/Chips.java
@@ -3,7 +3,19 @@ package com.pluralsight.orderitems;
 import com.pluralsight.com.pluralsight.Interface;
 
 public class Chips implements Interface {
+    private String chipSelection;
 
+    public Chips(String chipSelection) {
+        this.chipSelection = chipSelection;
+    }
+
+    public String getChipSelection() {
+        return chipSelection;
+    }
+
+    public void setChipSelection(String chipSelection) {
+        this.chipSelection = chipSelection;
+    }
 
     @Override
     public double getPrice() {
--- src/main/java/com/pluralsight/toppings/Topping.java
+++ src/main/java/com/pluralsight/toppings/Topping.java
@@ -3,6 +3,7 @@ package com.pluralsight.toppings;
 import com.pluralsight.com.pluralsight.Interface;
 
 public class Topping implements Interface {
+
     @Override
     public double getPrice() {
         return 0;
