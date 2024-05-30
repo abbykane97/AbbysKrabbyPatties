e55a0611918063d45e7ae8a27ecb71fb12687d3a
abbkane97 <kanehaby20@gmail.com>
2024-05-30 10:39:26 -0400

    Completed Order class

--- src/main/java/com/pluralsight/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/com/pluralsight/Order.java
@@ -1,4 +1,54 @@
 package com.pluralsight.com.pluralsight;
 
+import com.pluralsight.orderitems.Chips;
+import com.pluralsight.orderitems.Drink;
+import com.pluralsight.orderitems.Sandwich;
+
+import java.util.List;
+
 public class Order {
+    private List<Sandwich> sandwichList;
+    private List<Drink> drinks;
+    private List<Chips> chips;
+    private double orderTotal;
+
+    public Order(List<Sandwich> sandwichList, List<Drink> drinks, List<Chips> chips, double orderTotal) {
+        this.sandwichList = sandwichList;
+        this.drinks = drinks;
+        this.chips = chips;
+        this.orderTotal = orderTotal;
+
+    }
+
+    public List<Sandwich> getSandwichList() {
+        return sandwichList;
+    }
+
+    public void setSandwichList(List<Sandwich> sandwichList) {
+        this.sandwichList = sandwichList;
+    }
+
+    public List<Drink> getDrinks() {
+        return drinks;
+    }
+
+    public void setDrinks(List<Drink> drinks) {
+        this.drinks = drinks;
+    }
+
+    public List<Chips> getChips() {
+        return chips;
+    }
+
+    public void setChips(List<Chips> chips) {
+        this.chips = chips;
+    }
+
+    public double getOrderTotal() {
+        return orderTotal;
+    }
+
+    public void setOrderTotal(double orderTotal) {
+        this.orderTotal = orderTotal;
+    }
 }
